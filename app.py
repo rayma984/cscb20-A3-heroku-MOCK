335ad93c7f713b59dc6ae40ee71a21f4bd870010
rayma984 <moyraymond984@gmail.com>
2022-04-06 22:38:05 -0400

    sfgrdd

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from datetime import datetime, timedelta
+from itertools import product
 from flask import Flask, render_template, url_for, flash, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy import text # textual queries
 #rom sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-#from sqlalchemy import create_engine
+
+# ATTENTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+#     You are currently viewing an unfinished product
+#     PLEASE review TODOs in app.py functions
+#     AND review table schema changes in comments before
+#     removing this VERY IMPORTANT MESSAGE
+
+# ATTENTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 hush_hush = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
 #ripped off of flask's site for an example of a good secret key
@@ -14,15 +23,13 @@ hush_hush = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
 students = set()
 instructors = set()
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 app.config['SECRET_KEY'] = hush_hush
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ass3.db'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 8) #change as fit
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ass3.db'
 #engine = create_engine('sqlite:///ass3.db')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
 
 class Account(db.Model):
     __tablename__ = 'Account'
@@ -31,7 +38,7 @@ class Account(db.Model):
     password = db.Column(db.String(20), nullable = False)
     email = db.Column(db.String(100), unique=True, nullable=False) #doesnt need unique
     type = db.Column(db.String(10), nullable=False)
-
+    
     def __repr__(self):
         return f"Account('{self.username}', '{self.email}, {self.password}, {self.type}')"
 
@@ -53,7 +60,7 @@ class Marks(db.Model):
     __tablename__ = 'Marks'
     student_id = db.Column(db.Integer, db.ForeignKey('Student.Student_id'), nullable = False, primary_key = True)
     instructor_id = db.Column(db.Integer, db.ForeignKey('Instructor.Instructor_id'), nullable = False)
-    accessment = db.Column(db.String(20), nullable = False, primary_key = True)
+    assessment = db.Column(db.String(20), nullable = False, primary_key = True)
     grade = db.Column(db.Integer, nullable = False)
     #spelling error: accessment -> assessment 
 
@@ -68,13 +75,13 @@ class Feedback(db.Model):
 
 class Remark(db.Model):
     __tablename__ = 'Remark'
-    accessment = db.Column(db.String(20),db.ForeignKey('Marks.accessment'), nullable = False, primary_key = True)
+    assessment = db.Column(db.String(20),db.ForeignKey('Marks.assessment'), nullable = False, primary_key = True)
     student_id = db.Column(db.Integer, db.ForeignKey('Student.Student_id'), nullable = False, primary_key = True)
-    blurb = db.Column(db.String(100), unique = False, nullable = False)
+    blurb = db.Column(db.String(100), unique = False, nullable = True)
 
 class RemarkRequest():
     def __init__(self, input):
-        self.assessment = input.accessment #spelling
+        self.assessment = input.assessment  #spelling
         self.student = get_name_from_id(input.student_id)
         self.blurb = input.blurb
 
@@ -82,7 +89,7 @@ class Mark():
     def __init__(self, input):
         self.student = get_name_from_id(input.student_id)
         self.instructor = get_name_from_id(input.instructor_id)
-        self.assessment = input.accessment #spelling
+        self.assessment = input.assessment  #spelling
         self.grade = input.grade
 
 """
@@ -113,13 +120,6 @@ r4 = db.session.query(Person).filter(Person.username.like('P%'), Person.id.in_([
 for person in r4:
     print(person.username)
 
-#raw Query
-print('*******Raw Query Execution*******')
-sql = text('select * from Notes')
-result = db.engine.execute(sql)
-for r in result:
-    print(r['title'])
-
 """
 # ROUTING FOR NAVBAR
 
@@ -148,7 +148,7 @@ def assignments():
 def labs():
     return render_template('labs.html')
 
-#new pages for A3
+#new pages for A3 ---------------------------------------------
 @app.route('/stu_home')
 def stu_home():
     pagename = 'Home Page'
@@ -169,7 +169,10 @@ def submit_feedback():
         instructor = request.form['instructor']
         feedback = (q1,q2,q3,q4,instructor)
         add_feedback(feedback)
+        flash("feedback submitted!", "success")
         return render_template('submit_feedback.html', pagename = pagename, profs=profs)
+
+
 
 @app.route('/view_marks', methods = ['GET', 'POST'])
 def view_marks():
@@ -182,10 +185,13 @@ def view_marks():
     else: #POST
         assessment = request.form['assessment']
         reason = request.form[assessment]
-        remark_req = (assessment, id, reason)
+        
+        remark_req = (assessment, id,reason)
         add_remark(remark_req)
         return render_template('view_marks.html', pagename = pagename, query_marks=query_marks)
 #TODO fix issue of assessments past the first not being accepted
+#TODO check if there already exists a remark req in the db (same student same assessment), 
+#           if so, flash a rejection message (only one remark per assessment)
 
 @app.route('/instr_home')
 def instr_home():
@@ -212,6 +218,8 @@ def view_feedback():
         id = get_id_from_name(prof_name)
         query_feedbacks = get_feedback(id)
         return render_template('view_feedback.html', pagename = pagename, query_feedbacks=query_feedbacks)
+#TODO the css file needs to center the table created by jinja code
+
 
 @app.route('/view_remark', methods = ['GET', 'POST'])
 def view_remark():
@@ -223,6 +231,8 @@ def view_remark():
             remark = RemarkRequest(requests)
             remarks.add(remark)
         return render_template('view_remark.html', pagename = pagename, remarks=remarks)
+#TODO center the table created by jinja in css file 
+
 
 @app.route('/enter_marks', methods = ['GET', 'POST'])
 def enter_marks():
@@ -234,6 +244,18 @@ def enter_marks():
         assessment = request.form['assessment']
         grade = request.form['grade']
 
+        #check if the student is real
+        flag = False
+        students = get_all_stus()
+        for stu in students:
+            if stu.username == student:
+                flag = True #means the student exists
+                break
+
+        if(flag == False):
+            flash("Student does not exist!", "error")
+            return render_template('enter_marks.html', pagename = pagename) #happens when invalid student name
+
         stu_id = get_id_from_name(student)
         instr_id = get_id_from_name(session['name'])
 
@@ -241,17 +263,20 @@ def enter_marks():
         student_marks = query_student_marks(stu_id)
 
         for mark in student_marks:
-            if(mark.accessment == assessment): #db spelling error see Mark class
-                #give the user a flash message for remark processed
+            if(mark.assessment  == assessment ): #db spelling error see Mark class
+                
                 mark.grade = grade #this updates the info
                 db.session.commit()
+                flash("Assessment remarked!", "success")
                 return render_template('enter_marks.html', pagename = pagename)
                 break 
         
         input = (stu_id,instr_id,assessment,grade)
+
         add_mark(input)
         #add a flash message for mark added
-        return render_template('enter_marks.html', pagename = pagename) 
+        flash("mark added!")
+        return render_template('enter_marks.html', pagename = pagename)
 
 @app.route('/logout')
 def logout():
@@ -264,7 +289,7 @@ def register():
     pagename = 'Register'
     if request.method == 'GET':
         return render_template('register.html', pagename = pagename)
-    else:
+    else: #POST aka checking db to see if credentials are correct
         username = request.form['Username']
         email = request.form['Email']
 
@@ -277,34 +302,42 @@ def register():
             types
         )
 
-        add_users(reg_details)
-
         account = Account.query.filter_by(username = username).first()
-        acc_num = account.Account_id
-
-        if ((types == 'Student') == True):
-            students.add(username)
-            add_users_student(reg_details, acc_num)
-        else:
-            instructors.add(username)
-            add_users_instructor(reg_details, acc_num)
         
-
-        flash('Registration Successful! Please login now:')
-        return redirect(url_for('login'))
+        #if account with this name appeared in db --> username is already taken
+        if account:
+            flash("Username has already be taken!", "error") #ASSUMES WE CAN REBOOT DB (if not include email here)
+            return redirect(url_for('register'))
+        #otherwise, successful registration
+        else:
+            add_users(reg_details)
+            #add the info to the related tables
+            account1 = Account.query.filter_by(username = username).first()
+            acc_num = account1.Account_id
+            if ((types == 'Student') == True):
+                students.add(username)
+                add_users_student(reg_details, acc_num)
+            else:
+                instructors.add(username)
+                add_users_instructor(reg_details, acc_num)
+        
+            flash('Registration Successful! Please login now:', "success")
+            return redirect(url_for('login'))
+#TODO add error checking when no inputs are entered for each inut field
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
         if 'name' in session:
-            flash('already logged in!!') #this doesnt work lol
+            flash('already logged in!!')
 
             #which page should be shown?
-            if ('name' in students):
+            if (session['type'] == 'Student'):
                 return redirect(url_for('stu_home'))
-            elif ('name' in instructors):
+            elif (session['type'] == 'Instructor'):
                 return redirect(url_for('instr_home'))
             else:
+                flash("but we couldn't retreive account type, please login again")
                 return render_template('login.html')
         else:
             return render_template('login.html')
@@ -319,6 +352,7 @@ def login():
             return render_template('login.html')
         #if user is recognised
         else:
+            session.pop('name', default = None) #reset session
             session['name'] = username
             session['type'] = account.type
             
@@ -357,13 +391,13 @@ def add_users_instructor(reg_details, acc_num):
 #add a mark to the Mark table
 def add_mark(details):
     mark = Marks(student_id = details[0], instructor_id =  details[1],
-    accessment = details[2], grade =  details[3]) #db spelling error see Mark class
+    assessment  = details[2], grade =  details[3]) #db spelling error see Mark class
     db.session.add(mark)
     db.session.commit()
 
 #add a remark to the Remark table
 def add_remark(details):
-    remark = Remark(accessment = details[0], student_id =  details[1],
+    remark = Remark(assessment  = details[0], student_id =  details[1],
     blurb = details[2]) #db spelling error see Remark class
     db.session.add(remark)
     db.session.commit()
@@ -372,6 +406,11 @@ def add_remark(details):
 def get_all_profs():
     profs = Instructor.query.all()
     return profs
+
+#getting list of students  from Student table
+def get_all_stus():
+    stu = Student.query.all()
+    return stu
 
 #getting all the marks in the database
 def get_all_marks():
